074a6c971328cd82b3b449e71a177cc1482e2624
Brighton Sikarskie <brighton.sikarskie@tuta.io>
2020-12-08 23:54:33 -0600

    testing

---
+++ pre-cal/target/rls/debug/deps/pre_cal-e1194a68761c366f.d
@@ -0,0 +1,5 @@
+/home/god/github/Rust-Projects/pre-cal/target/rls/debug/deps/pre_cal-e1194a68761c366f.rmeta: src/main.rs
+
+/home/god/github/Rust-Projects/pre-cal/target/rls/debug/deps/pre_cal-e1194a68761c366f.d: src/main.rs
+
+src/main.rs:
