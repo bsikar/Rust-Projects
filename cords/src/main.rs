17c3a2a75043392af505e87cefef9589588e8d09
Brighton Sikarskie <brighton.sikarskie@tuta.io>
2021-03-07 12:46:52 -0600

    Update main.rs

--- cords/src/main.rs
+++ cords/src/main.rs
@@ -4,7 +4,7 @@ fn main() {
     let mut center_point = String::new();
     std::io::stdin()
         .read_line(&mut center_point)
-        .expect("Couldn't read line");
+        .expect("Failed to read line");
 
     let center_point = {
         let center_point = center_point
@@ -17,5 +17,25 @@ fn main() {
         (center_point[0], center_point[1])
     };
 
-    println!("{:?}", center_point);
+    println!("Enter either the radius or the diameter (enter `r=' or `d=')");
+
+    let mut radius = String::new();
+    std::io::stdin()
+        .read_line(&mut radius)
+        .expect("Failed to read line");
+
+    let radius = {
+        let radius = radius
+            .trim()
+            .split_whitespace()
+            .collect::<String>()
+            .to_lowercase();
+        match &radius[..2] {
+            "r=" => radius[2..].parse::<f64>().unwrap(),
+            "d=" => radius[2..].parse::<f64>().unwrap()/2.,
+            _ => unreachable!()
+        }
+    };
+
+    println!("{}^2 = (y + {})^2 + (x + {})^2", radius, center_point.1, center_point.0);
 }
