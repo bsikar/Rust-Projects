8e950a8965d1fd654a9cda2412f60e66634316d7
Brighton Sikarskie <brighton.sikarskie@tuta.io>
2021-04-17 23:46:10 -0500

    Update food.rs

--- snake_gui/src/food.rs
+++ snake_gui/src/food.rs
@@ -62,8 +62,8 @@ impl Food {
     pub fn new(size: Size) -> Food {
         Food {
             position: Position {
-                x: thread_rng().gen_range(1..(size.width / (BLOCK_SIZE * 2.0)) as u32),
-                y: thread_rng().gen_range(1..(size.height / (BLOCK_SIZE * 2.0)) as u32),
+                x: thread_rng().gen_range(0..=(size.width / (BLOCK_SIZE * 2.0)) as i32),
+                y: thread_rng().gen_range(0..=(size.height / (BLOCK_SIZE * 2.0)) as i32),
             },
         }
     }
@@ -72,8 +72,8 @@ impl Food {
     pub fn spawn(&mut self, size: Size, snake: &Snake) {
         while snake.tail.contains(&self.position) {
             self.position = Position {
-                x: thread_rng().gen_range(1..(size.width / (BLOCK_SIZE * 2.0)) as u32),
-                y: thread_rng().gen_range(1..(size.height / (BLOCK_SIZE * 2.0)) as u32),
+                x: thread_rng().gen_range(0..=(size.width / (BLOCK_SIZE * 2.0)) as i32),
+                y: thread_rng().gen_range(0..=(size.height / (BLOCK_SIZE * 2.0)) as i32),
             };
         }
     }
